bbaee41487aa7eeaaf3006bd9ffc56b9ceff9b70
Jason Favrod <lakona808@gmail.com>
2014-12-07 21:50:57 -1000

    Added Header File for srvr.cc
    Also edited srvr.cc; I removed unused includes.

---
+++ srvr.h
@@ -0,0 +1,11 @@
+#ifndef SRVR_H
+#define SRVR_H
+
+#define LISTENQ 1
+#define MAXLINE 4096
+#define MAXSOCKADDR 128
+#define BUFFSIZE 8192
+#define SERV_PORT 9878
+#define SERV_PORT_STR "9878"
+
+#endif
